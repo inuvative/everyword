9699f9d40eef75d5de8ebc259d080508b2b2c78a
dsimmons <derek.simmons@sensus.com>
2018-04-03 11:59:08 -0400

    Fix port

--- server/app.js
+++ server/app.js
@@ -6,7 +6,6 @@
 
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-process.env.PORT = 9001;
 
 var express = require('express');
 var mongoose = require('mongoose');
